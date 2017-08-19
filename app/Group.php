1a85ab7e35a0896b68840935517dad6673925f98
hirokisan39 <expsharetoworld@gmail.com>
2017-08-19 22:32:50 +0900

    Add relation between user and group

--- app/Group.php
+++ app/Group.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     /**
+     * Get related user
+     */
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
+
+    /**
      * Get related related privilege
      */
     public function privilege()
--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get related related group
+     */
+    public function group()
+    {
+        return $this->belongsTo('App\Group');
+    }
 }
