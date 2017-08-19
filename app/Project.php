b1315890a6a20101f7f1025af9cebeb3b4d2a74c
hirokisan39 <expsharetoworld@gmail.com>
2017-08-19 21:49:40 +0900

    Add relation between service and project

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    /**
+     * Get related related service
+     */
+    public function services()
+    {
+        return $this->belongsTo('App\Project');
+    }
 }
--- app/Service.php
+++ app/Service.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    /**
+     * Get related project
+     */
+    public function projects()
+    {
+        return $this->hasMany('App\Project');
+    }
 }
